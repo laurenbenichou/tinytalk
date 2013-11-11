a5b1f64be93576133c7ffbf8c5c00c1f79e68764
laurenbenichou <laurenbenichou@gmail.com>
2013-11-10 16:44:44 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinytalk::Application.config.secret_token = '4d6a6f6247babca73b7c0db7e8414486b009f85fac039b3a0eb8059331806c478d63bc1656902c3bb1c2ab29f52a060dd37ec98bec528ae2138d318b3cada835'
