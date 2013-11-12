4fa5aa3e1d388d30e4ab2fdb632dad3ecd1fa4fc
jeesong <jeewsong@gmail.com>
2013-11-11 16:33:44 -0800

    Set up testing for Ruby with rSpec

---
+++ spec/models/board_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Board do
+  let(:board) {Board.create(title: "board1")}
+
+  it 'should have an id' do
+    board.should respond_to(:id)
+    board.id.should_not == nil
+  end
+
+  it 'should have a user_id' do
+    board.should respond_to(:user_id)
+    board.user_id.should_not == nil
+  end
+
+end
