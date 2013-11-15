9f47e57b07e4c6fab7e444b497db8591df7f1421
jeesong <jeewsong@gmail.com>
2013-11-14 16:03:40 -0800

    Added comments to text posts

--- app/controllers/text_pins_controller.rb
+++ app/controllers/text_pins_controller.rb
@@ -16,6 +16,9 @@ class TextPinsController < ApplicationController
   # GET /text_pins/1.json
   def show
     @text_pin = current_user.text_pins.find(params[:id])
+    @board = params[:board_id]
+    @comment = Comment.new
+    @comments = Comment.where(commentable_type: 'Text_Pin', commentable_id: @text_pin)
 
     respond_to do |format|
       format.html # show.html.erb
@@ -37,6 +40,7 @@ class TextPinsController < ApplicationController
 
   # GET /text_pins/1/edit
   def edit
+    @board = Board.find(params[:board_id])
     @text_pin = current_user.text_pins.find(params[:id])
   end
 
@@ -59,11 +63,12 @@ class TextPinsController < ApplicationController
   # PUT /text_pins/1
   # PUT /text_pins/1.json
   def update
+    @board = Board.find(params[:board_id])
     @text_pin = current_user.text_pins.find(params[:id])
 
     respond_to do |format|
       if @text_pin.update_attributes(params[:text_pin])
-        format.html { redirect_to @text_pin, notice: 'Text pin was successfully updated.' }
+        format.html { redirect_to board_text_pin_path(@board, @text_pin), notice: 'Text pin was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -75,11 +80,12 @@ class TextPinsController < ApplicationController
   # DELETE /text_pins/1
   # DELETE /text_pins/1.json
   def destroy
+    @board = params[:board_id]
     @text_pin = current_user.text_pins.find(params[:id])
     @text_pin.destroy
 
     respond_to do |format|
-      format.html { redirect_to text_pins_url }
+      format.html { redirect_to board_path(@board) }
       format.json { head :no_content }
     end
   end
