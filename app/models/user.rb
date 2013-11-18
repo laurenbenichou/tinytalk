0fd0b85c794f6224fd10854ebb25a699186b2c81
laurenbenichou <laurenbenichou@gmail.com>
2013-11-18 03:14:18 -0800

    fixed sidebar word wrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
 
   attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "missing.jpg"
+  has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/assets/missing.jpg"
 
   validates :username, uniqueness: true
 
